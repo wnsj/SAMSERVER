8cd2a55b796a9950cd7e8c497ea0a8bcc9ea9957
1332165192 <1332165192@qq.com>
2019-09-10 09:30:25 +0800

    科室接口修改2

---
+++ src/main/java/com/jiubo/sam/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.jiubo.sam.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @desc:Swagger配置类
+ * @date: 2019-09-10 08:31
+ * @author: dx
+ * @version: 1.0
+ */
+@Configuration
+//@EnableSwagger2
+public class SwaggerConfig {
+
+    //public
+}
