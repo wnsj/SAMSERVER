4cae39870ef3e89f36716f9128742819679c065f
1332165192 <1332165192@qq.com>
2019-09-10 09:28:36 +0800

    科室接口修改

--- src/main/java/com/jiubo/sam/service/impl/DepartmentServiceImpl.java
+++ src/main/java/com/jiubo/sam/service/impl/DepartmentServiceImpl.java
@@ -1,5 +1,9 @@
 package com.jiubo.sam.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.ISqlSegment;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.segments.MergeSegments;
 import com.jiubo.sam.bean.DepartmentBean;
 import com.jiubo.sam.dao.DepartmentDao;
 import com.jiubo.sam.exception.MessageException;
@@ -12,7 +16,7 @@ import java.util.List;
 
 /**
  * <p>
- *  科室服务实现类
+ * 科室服务实现类
  * </p>
  *
  * @author dx
@@ -31,11 +35,20 @@ public class DepartmentServiceImpl extends ServiceImpl<DepartmentDao, Department
 
     @Override
     public void addDepartment(DepartmentBean departmentBean) throws MessageException {
-       if(departmentDao.insert(departmentBean) <= 0) throw new MessageException("操作失败!");
+        QueryWrapper<DepartmentBean> queryWrapper = new QueryWrapper<DepartmentBean>();
+        queryWrapper.eq(true, "NAME", departmentBean.getName());
+        List<DepartmentBean> departmentBeans = departmentDao.selectList(queryWrapper);
+        if(departmentBeans.size() > 0)throw new MessageException("科室名不能重复!");
+        if (departmentDao.insert(departmentBean) <= 0) throw new MessageException("操作失败!");
     }
 
     @Override
     public void updateDepartment(DepartmentBean departmentBean) throws MessageException {
-        if(departmentDao.updateById(departmentBean) <= 0)throw new MessageException("操作失败!");
+        QueryWrapper<DepartmentBean> queryWrapper = new QueryWrapper<DepartmentBean>();
+        queryWrapper.eq(true, "NAME", departmentBean.getName());
+        queryWrapper.ne("DEPT_ID", departmentBean.getDeptId());
+        List<DepartmentBean> departmentBeans = departmentDao.selectList(queryWrapper);
+        if(departmentBeans.size() > 0)throw new MessageException("科室名不能重复!");
+        if (departmentDao.updateById(departmentBean) <= 0) throw new MessageException("操作失败!");
     }
 }
