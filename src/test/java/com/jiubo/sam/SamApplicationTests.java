5785260e970dbbb7ac70fe1710195eb7c56a9e62
1332165192 <1332165192@qq.com>
2020-08-21 17:17:52 +0800

    交费统计bug修改

--- src/test/java/com/jiubo/sam/SamApplicationTests.java
+++ src/test/java/com/jiubo/sam/SamApplicationTests.java
@@ -19,15 +19,15 @@ public class SamApplicationTests {
 
     public static void main(String[] args) {
         Vector v = new Vector();
-        for (int i = 0; i < 25; i++)
+        for (int i = 0; i < 25; i++) {
             v.add(new String[1 * 1024 * 1024]);
 
-        System.out.println("Xmx=" + Runtime.getRuntime().maxMemory() / 1024.0 / 1024 + "M");
+            System.out.println("Xmx=" + Runtime.getRuntime().maxMemory() / 1024.0 / 1024 + "M");
 
-        System.out.println("free mem=" + Runtime.getRuntime().freeMemory() / 1024.0 / 1024 + "M");
+            System.out.println("free mem=" + Runtime.getRuntime().freeMemory() / 1024.0 / 1024 + "M");
 
-        System.out.println("total mem=" + Runtime.getRuntime().totalMemory() / 1024.0 / 1024 + "M");
-
+            System.out.println("total mem=" + Runtime.getRuntime().totalMemory() / 1024.0 / 1024 + "M");
+        }
 
     }
 
