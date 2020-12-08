3e2ef22597a9e2edab044563017a81fe1ecf60dc
gitee <2724364263@qq.com>
2020-12-08 14:40:56 +0800

    添加操作项目日志，项目默认单价设置

--- src/test/java/com/jiubo/sam/dao/PayserviceDaoTest.java
+++ src/test/java/com/jiubo/sam/dao/PayserviceDaoTest.java
@@ -1,16 +1,7 @@
 package com.jiubo.sam.dao;
 
-import com.jiubo.sam.bean.PayserviceBean;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.*;
 
 /**
  * @desc:
