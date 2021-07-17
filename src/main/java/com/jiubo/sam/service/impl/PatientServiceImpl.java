05d519363de5e620a8f3a92a521a609fa7faafa8
sunyl-5664 <lx5664@hotmail.com>
2021-07-17 10:38:03 +0800

    启动项时间查询

--- src/main/java/com/jiubo/sam/service/impl/PatientServiceImpl.java
+++ src/main/java/com/jiubo/sam/service/impl/PatientServiceImpl.java
@@ -263,26 +263,28 @@ public class PatientServiceImpl extends ServiceImpl<PatientDao, PatientBean> imp
             if (outHosp==null|| outHosp.equals("")){
                 throw new MessageException("出院时间必填");
             }
-//            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-//            long beginUseTime = sdf.parse(outHosp).getTime();
-//            Long endTimeLong = beginUseTime - 86400000;
-//            SimpleDateFormat sdfg=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-//            String sd = sdfg.format(new Date(Long.parseLong(String.valueOf(endTimeLong))));      // 时间戳转换成时间
-//
-//            String hospNum = patientBean.getHospNum();
-//            List<PaPayserviceBean> paPayserviceBeans = paPayserviceDao.selectPaPayService(hospNum);
-//            if (paPayserviceBeans.size()>=1){
-//                //有开启的项目
-//
-//                for (PaPayserviceBean paPayserviceBean : paPayserviceBeans) {
-//                    String isUse = paPayserviceBean.getIsUse();
-//                    if (!isUse.equals("0")){
-//                        paPayserviceBean.setIsUse("0");
-//                        paPayserviceBean.setEndDate(sd);
-//                    }
-//                    paPayserviceDao.updateById(paPayserviceBean);
-//                }
-//            }
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            Long beginUseTime = sdf.parse(outHosp).getTime();
+            Long endTimeLong = beginUseTime - 86400000;
+            SimpleDateFormat sdfg=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+            String sd = sdfg.format(new Date(Long.parseLong(String.valueOf(endTimeLong))));
+
+            String hospNum = patientBean.getHospNum();
+            List<PaPayserviceBean> paPayserviceBeans = paPayserviceDao.selectPaPayService(hospNum);
+            if (paPayserviceBeans.size()>=1){
+                //有开启的项目
+
+                for (PaPayserviceBean paPayserviceBean : paPayserviceBeans) {
+                    String isUse = paPayserviceBean.getIsUse();
+                    if (!isUse.equals("0")){
+                        paPayserviceBean.setIsUse("0");
+                        paPayserviceBean.setEndDate(sd);
+                    }
+                    paPayserviceDao.updateById(paPayserviceBean);
+                }
+            }
+
         }
 
         //添加出入院记录
